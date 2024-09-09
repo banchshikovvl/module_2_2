7748f52a5c9445e6a6cfe18cadedaf86d737acd4
Vlad <banshchikovvl@yandex.ru>
2024-09-09 21:56:38 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+#Пункты задачи:
+#Если все числа равны между собой, то вывести 3
+#Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
+#Если равных чисел среди 3-х вообще нет, то вывести 0
+
+first = int(input('Введите первое число: '))
+second = int(input('Введите первое число: '))
+third = int(input('Введите первое число: '))
+
+if first != second and first != third and second != third:
+    print(0)
+elif first == second and first == third and second == third:
+    print(3)
+else:
+    print(2)
