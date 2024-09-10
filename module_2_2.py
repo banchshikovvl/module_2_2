97da4ce3854641ee761165267681f15906e98813
Vlad <banshchikovvl@yandex.ru>
2024-09-10 16:06:00 +0700

    Update module_2_2.py
    Скорректировал комментарии к вводу чисел

--- module_2_2.py
+++ module_2_2.py
@@ -4,8 +4,8 @@
 #Если равных чисел среди 3-х вообще нет, то вывести 0
 
 first = int(input('Введите первое число: '))
-second = int(input('Введите первое число: '))
-third = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
 
 if first != second and first != third and second != third:
     print(0)
